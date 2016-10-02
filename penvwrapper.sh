f507dda3adc17d2c38285bd35448e3b1e5f0fa23
Sam Brightman <sam.brightman@gmail.com>
2016-10-02 17:08:49 +0200

    Aim for POSIX compliance but don't give up on 'local' yet

--- penvwrapper.sh
+++ penvwrapper.sh
@@ -1,5 +1,5 @@
-# -*- mode: bash-script -*-
-#
+# shellcheck disable=SC2039
+
 # Shell functions to act as wrapper for Joe Topjian's penv
 # (https://github.com/jtopjian)
 #
@@ -7,61 +7,62 @@
 #
 # Copyright 2016 Sam Brightman
 
-if [[ -z "${PWORKON_HOME}" ]]; then
+if [ -z "${PWORKON_HOME}" ]; then
     export PWORKON_HOME=~/.pvirtualenvs
 fi
 
 mkdir -p "${PWORKON_HOME}"
 
-if [[ -z "${PENVWRAPPER_PENV}" ]]
+if [ -z "${PENVWRAPPER_PENV}" ]
 then
     PENVWRAPPER_PENV="penv.pl"
 fi
 
-function pmkvirtualenv() {
+pmkvirtualenv() {
     local name=$1 && shift
-    "$PENVWRAPPER_PENV" "${PWORKON_HOME}/${name}" "$@"
+    "$PENVWRAPPER_PENV" "${PWORKON_HOME}/${name:?You must provide a name.}" "$@"
     pworkon "${name}"
 }
 
-function pworkon() {
+pworkon() {
     local name=$1 && shift
-    if [[ -z "${name}" ]]; then
+    if [ -z "${name}" ]; then
         plsvirtualenv
     elif [ -s "${PWORKON_HOME}/${name}/bin/activate" ]; then
-        source "${PWORKON_HOME:?PWORKON_HOME is not set, please set it}/${name}/bin/activate"
+        # shellcheck disable=SC1090 
+        . "${PWORKON_HOME:?PWORKON_HOME is not set, please set it.}/${name}/bin/activate"
     else
-        echo "${name} is not a pvirtualenv"
+        echo "${name} is not a pvirtualenv" && return 1
     fi
 }
 
-function plsvirtualenv() {
-    local nullglob; nullglob=$(shopt -p nullglob)
-    shopt -s nullglob
-    for d in ${PWORKON_HOME}/*; do
-        basename "$d"
+plsvirtualenv() {
+    local penv
+    for penv in ${PWORKON_HOME}/*/; do
+        if [ -d "${penv}" ]; then
+            basename "$penv"
+        fi
     done
-    $nullglob
 }
 
-function pcdvirtualenv() {
+pcdvirtualenv() {
     local name=$1 && shift
-    if [[ -n "${name}" && -d "${PWORKON_HOME}/${name}" ]]; then
-        cd "${PWORKON_HOME:?PWORKON_HOME is not set, please set it}/${name}"
-    elif [[ -n "${VIRTUAL_ENV}" && "$(dirname "${VIRTUAL_ENV}")" == "${PWORKON_HOME}" ]]; then
-        cd "${VIRTUAL_ENV}"
+    if [ -n "${name}" ] && [ -d "${PWORKON_HOME}/${name}" ]; then
+        cd "${PWORKON_HOME:?PWORKON_HOME is not set, please set it.}/${name}" || return
+    elif [ -n "${VIRTUAL_ENV}" ] && [ "$(dirname "${VIRTUAL_ENV}")" = "${PWORKON_HOME}" ]; then
+        cd "${VIRTUAL_ENV}" || return
     elif [ -z "${name}" ]; then
-        cd "${PWORKON_HOME:?PWORKON_HOME is not set, please set it}"
+        cd "${PWORKON_HOME:?PWORKON_HOME is not set, please set it.}" || return
     else
-        echo "${name} is not a pvirtualenv"
+        echo "${name} is not a pvirtualenv" && return 1
     fi
 }
 
-function prmvirtualenv() {
+prmvirtualenv() {
     local name=$1 && shift
     if [ -d "${PWORKON_HOME}/${name:?Please specify an environment.}" ]; then
-        rm -rf "${PWORKON_HOME:?PWORKON_HOME is not set, please set it}/${name}"
+        rm -rf "${PWORKON_HOME:?PWORKON_HOME is not set, please set it.}/${name}"
     else
-        echo "${name} is not a pvirtualenv"
+        echo "${name} is not a pvirtualenv" && return 1
     fi
 }
